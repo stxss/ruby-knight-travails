48d510b02314ff9ccc5b142a4156444173fd5290
stxss <93729036+stxss@users.noreply.github.com>
2023-02-20 20:06:55 +0000

    Add knight_moves and test calls

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,14 @@
 require_relative "board"
 require_relative "knight_moves"
 
-chess_board = Board.new
+def knight_moves(starting, ending)
+    chess_board = Board.new
 
-chess_board.board.each do |i|
-    p i
+    knight =  Knight.new(chess_board.board, starting, ending)
+
 end
+
+# knight_moves([0, 0], [1, 2])
+# knight_moves([0, 0], [3, 3])
+knight_moves([3, 3], [0, 0])
+# knight_moves([3, 3], [4, 3])
