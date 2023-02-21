e6bc95dec1ef912849379ed88e02a98b8b7bf417
stxss <93729036+stxss@users.noreply.github.com>
2023-02-21 19:22:47 +0000

    Add calls to check some possible paths

--- lib/main.rb
+++ lib/main.rb
@@ -2,13 +2,24 @@ require_relative "board"
 require_relative "knight_moves"
 
 def knight_moves(starting, ending)
-    chess_board = Board.new
+  chess_board = Board.new
 
-    knight =  Knight.new(chess_board.board, starting, ending)
-
+  knight = Knight.new(chess_board.board, starting, ending)
+  path = knight.moves
+  if path.length < 2
+    puts "You made it in #{path.length - 1} move! Here's your path:"
+  else
+    puts "You made it in #{path.length - 1} moves! Here's your path:"
+  end
+  path.each do |i|
+    puts "[#{i.first},#{i.last}]"
+  end
 end
 
-# knight_moves([0, 0], [1, 2])
-# knight_moves([0, 0], [3, 3])
+knight_moves([0, 0], [1, 2])
+knight_moves([0, 0], [3, 3])
 knight_moves([3, 3], [0, 0])
-# knight_moves([3, 3], [4, 3])
+knight_moves([3, 3], [4, 3])
+knight_moves([0, 1], [6, 2])
+knight_moves([3, 2], [3, 7])
+knight_moves([0, 0], [7, 7])
